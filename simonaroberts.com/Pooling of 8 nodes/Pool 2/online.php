f72c53835073ca408c4067a52affc9dcbe837a94
root <wishcraft@users.sourceforge.net>
2020-10-12 01:08:11 +0000

    :: simonaroberts.com / ntp.labs.coop / ntp.snails.email :: Mon Oct 12 01:08:11 UTC 2020 :: NTP Service Backup ::

---
+++ simonaroberts.com/Pooling of 8 nodes/Pool 2/online.php
@@ -0,0 +1,7 @@
+ mysqli Object
+(
+    [client_version] => 70403
+    [connect_errno] => 1040
+    [connect_error] => Too many connections
+)
+Fatal: Unable to connect to database ~ (databasefactory.php:76)
