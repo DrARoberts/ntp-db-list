40f3bced48189efd3042d1126e96e0a2b405e6fe
root <wishcraft@users.sourceforge.net>
2020-10-12 01:02:06 +0000

    :: simonaroberts.com / ntp.labs.coop / ntp.snails.email :: Mon Oct 12 01:02:06 UTC 2020 :: NTP Service Backup ::

--- ntp.labs.coop/offcompanies.php
+++ ntp.labs.coop/offcompanies.php
@@ -1,7 +1,60 @@
- mysqli Object
-(
-    [client_version] => 70403
-    [connect_errno] => 1040
-    [connect_error] => Too many connections
-)
-Fatal: Unable to connect to database ~ (databasefactory.php:76)
+<?php
+
+ return array (
+  'izbi-republic-of-moldova' => 
+  array (
+    'companyname' => 'Izbi, Republic of Moldova',
+    'companyemail' => 'webmaster at izbi dot com',
+    'companyurl' => 'http://izbi.com',
+    'companyrbn' => '',
+    'companyrbntype' => '',
+    'key' => '6988880ba3dadf82eaee6b223f3035ca',
+  ),
+  'fallon-united-states' => 
+  array (
+    'companyname' => 'Fallon, United States',
+    'companyemail' => 'webmaster at fallon dot com',
+    'companyurl' => 'http://fallon.com',
+    'companyrbn' => '',
+    'companyrbntype' => '',
+    'key' => '06b7683225ae9e205979cfe156e74b24',
+  ),
+  'university-of-san-diego-united-states' => 
+  array (
+    'companyname' => 'University of San Diego(United States)',
+    'companyemail' => 'webmaster at sandiego dot edu',
+    'companyurl' => '',
+    'companyrbn' => '',
+    'companyrbntype' => '',
+    'key' => '2894fadc3e52636f14fb4ab6de3b3907',
+  ),
+  'universitas-pancasila-indonesia' => 
+  array (
+    'companyname' => 'Universitas Pancasila(Indonesia)',
+    'companyemail' => 'feedback at univpancasila dot ac dot id',
+    'companyurl' => '',
+    'companyrbn' => '',
+    'companyrbntype' => '',
+    'key' => '693522f06c0c593e5d971dea3db805d4',
+  ),
+  'rafn-united-states' => 
+  array (
+    'companyname' => 'Rafn, United States',
+    'companyemail' => 'webmaster at rafn dot com',
+    'companyurl' => 'http://rafn.com',
+    'companyrbn' => '',
+    'companyrbntype' => '',
+    'key' => '3922e39f1bce13fa8b26590a16decd4a',
+  ),
+  'texas-a-m-university-college-station-united-states' => 
+  array (
+    'companyname' => 'Texas A&M University - College Station(United States)',
+    'companyemail' => 'feedback at tamu dot edu',
+    'companyurl' => '',
+    'companyrbn' => '',
+    'companyrbntype' => '',
+    'key' => '6fb0347ffb2012cce36893d08e2d8e9c',
+  ),
+);
+
+?>
